6a0f7726faa7169815c5865328fd33635fea6650
Lionel Miranda <bibliothecalionel@gmail.com>
2023-02-06 18:43:14 -0400

    fix: random button lint

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const rdnColor = document.querySelector('#button-random-color');
+const rdnBtnColor = document.querySelector('#button-random-color');
 const colorsPalette = document.querySelectorAll('.color');
 
-const getRandomColor = () => Math.floor(Math.random() * 255);
-
-rdnColor.addEventListener('click', () => {
-  colorsPalette[1].style.backgroundColor = `rgb(${getRandomColor()},${getRandomColor()},${getRandomColor()})`;
-  colorsPalette[2].style.backgroundColor = `rgb(${getRandomColor()},${getRandomColor()},${getRandomColor()})`;
-  colorsPalette[3].style.backgroundColor = `rgb(${getRandomColor()},${getRandomColor()},${getRandomColor()})`;
+rdnBtnColor.addEventListener('click', () => {
+  const randomNumber = () => Math.floor(Math.random() * 255);
+  const randomRgb = () =>
+    `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
+  for (let index = 1; index < 4; index += 1) {
+    colorsPalette[index].style.backgroundColor = randomRgb();
+  }
 });
