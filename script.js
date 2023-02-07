11acd93d08d1b99a3f42641ff2c6247d948985d5
Lionel Miranda <bibliothecalionel@gmail.com>
2023-02-07 08:44:19 -0400

    senhor, parece até q eu matei aula de web sotorage

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-const rdnBtnColor = document.querySelector('#button-random-color');
-const colorsPalette = document.querySelectorAll('.color');
+const paleta = document.querySelectorAll('.color');
 
-rdnBtnColor.addEventListener('click', () => {
-  const randomNumber = () => Math.floor(Math.random() * 255);
-  const randomRgb = () =>
-    `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
-  for (let index = 1; index < 4; index += 1) {
-    colorsPalette[index].style.backgroundColor = randomRgb();
+// 5 - req 1
+
+function salvarNoLocal() {
+  const listaCores = [];
+  for (let i = 1; i < paleta.length; i += 1) {
+    listaCores[i] = paleta[i].style.backgroundColor;
   }
-});
+  const stg = localStorage;
+  stg.setItem('colorPalette', JSON.stringify(listaCores));
+}
+
+function restaurarNoLocal() {
+  const restaurarCores = JSON.parse(localStorage.getItem('colorPalette')) || [];
+
+  for (let i = 1; i < paleta.length; i += 1) {
+    paleta[i].style.backgroundColor = restaurarCores[i];
+  }
+}
+
+// 4
+
+function sortearCores() {
+  const nAleatorio = () => Math.floor(Math.random() * 255);
+  const gerarCores = () =>
+    `rgb(${nAleatorio()},${nAleatorio()},${nAleatorio()})`;
+
+  for (let i = 1; i < paleta.length; i += 1) {
+    paleta[i].style.backgroundColor = gerarCores();
+  }
+  salvarNoLocal();
+}
+
+const btnAleato = document.querySelector('#button-random-color');
+btnAleato.addEventListener('click', sortearCores);
+
+// 5 req 2
+
+window.onload = () => restaurarNoLocal();
